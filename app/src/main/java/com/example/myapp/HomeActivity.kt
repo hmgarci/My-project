44079d2a79d6a221472b852c4eecf94ddff0db7a
hmgarci <hamaga97@gmail.com>
2019-04-27 09:49:29 -0500

    Termino carga de imagenes

--- app/src/main/java/com/example/myapp/HomeActivity.kt
+++ app/src/main/java/com/example/myapp/HomeActivity.kt
@@ -1,24 +1,81 @@
 package com.example.myapp
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.graphics.Palette
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var imageGalleryAdapter: ImageGalleryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        this.showName()
-
-
+        //this.showName()
+        val layoutManager = GridLayoutManager(this, 2)
+        recyclerView = findViewById(R.id.rv_images)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = layoutManager
+        imageGalleryAdapter = ImageGalleryAdapter(this, SetPhoto.getSunsetPhotos())
     }
-    companion object {
-        const val EXTRA_SUNSET_PHOTO = "SunsetPhotoActivity.EXTRA_SUNSET_PHOTO"
+    override fun onStart() {
+        super.onStart()
+        recyclerView.adapter = imageGalleryAdapter
     }
+    private inner class ImageGalleryAdapter(val context: Context, val sunsetPhotos: Array<SetPhoto>)
+        : RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>() {
 
-    private fun showName(){
-        var text=intent.getStringExtra("name").toString()
-        //textName?.setText("Welcome " + text)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageGalleryAdapter.MyViewHolder {
+            val context = parent.context
+            val inflater = LayoutInflater.from(context)
+            val photoView = inflater.inflate(R.layout.activity_image_detail, parent, false)
+            return MyViewHolder(photoView)
+        }
+        override fun onBindViewHolder(holder: ImageGalleryAdapter.MyViewHolder, position: Int) {
+            val sunsetPhoto = sunsetPhotos[position]
+            val imageView = holder.photoImageView
+            Picasso.get()
+                .load(sunsetPhoto.url)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .fit()
+                .into(imageView)
+
+        }
+
+        override fun getItemCount(): Int {
+            return sunsetPhotos.size
+        }
+        inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+            var photoImageView: ImageView = itemView.findViewById(R.id.iv_photo)
+
+            init {
+                itemView.setOnClickListener(this)
+            }
+
+            override fun onClick(view: View) {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val sunsetPhoto = sunsetPhotos[position]
+                    val intent = Intent(context, HomeActivity::class.java).apply {
+                        putExtra(ImageDetail.EXTRA_SUNSET_PHOTO, sunsetPhoto)
+                    }
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapp/ImageDetail.kt
+++ app/src/main/java/com/example/myapp/ImageDetail.kt
@@ -2,8 +2,10 @@ package com.example.myapp
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.graphics.Palette
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -13,51 +15,51 @@ import com.squareup.picasso.Picasso
 
 class ImageDetail : AppCompatActivity() {
 
+    companion object {
+        const val EXTRA_SUNSET_PHOTO = "ImageDetail.EXTRA_SUNSET_PHOTO"
+    }
+
+    private lateinit var imageView: ImageView
+    private lateinit var setPhoto: SetPhoto
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_detail)
+
+        setPhoto=intent.getParcelableExtra(ImageDetail.EXTRA_SUNSET_PHOTO)
+        imageView=findViewById(R.id.image)
     }
-    private inner class ImageGalleryAdapter(val context: Context, val sunsetPhotos: Array<SetPhoto>)
-        : RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageGalleryAdapter.MyViewHolder {
-            val context = parent.context
-            val inflater = LayoutInflater.from(context)
-            val photoView = inflater.inflate(R.layout.activity_image_detail, parent, false)
-            return MyViewHolder(photoView)
-        }
-        override fun onBindViewHolder(holder: ImageGalleryAdapter.MyViewHolder, position: Int) {
-            val sunsetPhoto = sunsetPhotos[position]
-            val imageView = holder.photoImageView
-            Picasso.get()
-                .load(sunsetPhoto.url)
-                .placeholder(R.drawable.placeholder)
-                .error(R.drawable.error)
-                .fit()
-                .into(imageView)
+    override fun onStart() {
+        super.onStart()
 
-        }
+        Picasso.get()
+            .load(setPhoto.url)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.error)
+            .fit()
+            .priority(Picasso.Priority.HIGH)
+            .into(imageView)
 
-        override fun getItemCount(): Int {
-            return sunsetPhotos.size
-        }
-        inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        Picasso.get()
+            .load(setPhoto.url)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.error)
+            .fit()
+            .priority(Picasso.Priority.LOW)
+            .into(imageView)
 
-            var photoImageView: ImageView = itemView.findViewById(R.id.iv_photo)
-
-            init {
-                itemView.setOnClickListener(this)
-            }
-
-            override fun onClick(view: View) {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    val sunsetPhoto = sunsetPhotos[position]
-                    val intent = Intent(context, HomeActivity::class.java).apply {
-                        putExtra(HomeActivity.EXTRA_SUNSET_PHOTO, sunsetPhoto)
-                    }
-                    startActivity(intent)
-                }
-            }
+        Picasso.get()
+            .load(setPhoto.url)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.error)
+            .fit()
+            .priority(Picasso.Priority.NORMAL)
+            .into(imageView)
+    }
+    fun onPalette(palette: Palette?) {
+        if (null != palette) {
+            val parent = imageView.parent.parent as ViewGroup
+            parent.setBackgroundColor(palette.getDarkVibrantColor(Color.GRAY))
         }
     }
     }
