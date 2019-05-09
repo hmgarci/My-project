f4f31663cb477e8cb6d47a6b187a16ed83e7778b
Harold Martinez Garcia <hmgarci@pb0b0835132.local>
2019-05-08 19:15:42 -0500

    Agrego autenticacion a firebase

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,15 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var auth=FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_login.setOnClickListener{ this.validateLogin()}
+        //btn_login.setOnClickListener{ this.validateLogin()}
         link_signup.setOnClickListener{processLinkSingup()}
 
     }
@@ -33,4 +39,30 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "ups! your info is not correct", Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun login(wiew:View){
+        var user= input_email.text.toString()
+        var pass= input_password.text.toString()
+
+        if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pass)){
+            auth.signInWithEmailAndPassword(user,pass)
+                .addOnCompleteListener(this){
+                    task ->
+                        if (task.isSuccessful){
+                            goToHome()
+                        }else{
+                            Toast.makeText(this@MainActivity, "¡ups! Los datos son incorrectos, revisalos e ingresalos de nuevo por favor", Toast.LENGTH_SHORT).show()
+                        }
+                }
+
+
+        }else{
+            Toast.makeText(this@MainActivity, "¡Hey! Completa los datos por favor", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun goToHome(){
+        var intent= Intent(this,HomeActivity::class.java)
+        startActivity(intent)
+    }
 }
+
